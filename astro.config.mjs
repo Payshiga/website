84ad44e965da5c51868fdd2e95eec09e45ad9e72
Ini_Jones <74598414+inijones@users.noreply.github.com>
2025-09-25 13:12:24 +0100

    Update with deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://payshiga.github.io/website/',
-
+  base: '/website/',   // 👈 this is critical for GitHub project pages
 });
